05aa947d1f71d34d68aae432827d27e5b58010cd
z <xiuxiuyifan@163.com>
2024-09-12 17:34:53 +0800

    feat: 大屏适配解决方案

---
+++ postcss.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    "plugins": {
+        "postcss-pxtorem": {
+            rootValue: 16, // 16px = 1rem
+            // unitPrecision: 5,
+            propList: ['*'],
+            // selectorBlackList: ['el-',], //
+            // replace: true,
+            // mediaQuery: false,
+            // minPixelValue: 0
+        }
+    }
+}
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div class="header"></div>
+    <div class="wrapper">
+      <div class="left"></div>
+      <div class="right"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  width: 1920px;
+  height: 1080px;
+  display: flex;
+  flex-direction: column;
+}
+.header{
+  height: 80px;
+  flex-shrink: 0;
+  border: 1px solid;
+}
+
+.wrapper{
+  padding: 20px;
+  display: flex;
+  flex: 1;
+  border: 1px solid;
+}
+.left{
+  width: 300px;
+  height: 924px;
+  border: 1px solid;
+}
+.right{
+  width: 1582px;
+  height: 835px;
+  border: 1px solid;
+}
+</style>
