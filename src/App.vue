251ad04afade02bf09fb5e95e182092edd634123
z <xiuxiuyifan@163.com>
2024-09-13 10:24:17 +0800

    feat: 大屏适配测试

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div class="container">
-    <div class="header"></div>
+    <div class="header">
+      <div class="l-name">xxxx管理平台</div>
+    </div>
     <div class="wrapper">
       <div class="left"></div>
       <div class="right"></div>
@@ -12,7 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
   </div>
 </template>
 
-<style scoped>
+<style lang="less" scoped>
 .container{
   width: 1920px;
   height: 1080px;
@@ -23,6 +25,11 @@ import HelloWorld from './components/HelloWorld.vue'
   height: 80px;
   flex-shrink: 0;
   border: 1px solid;
+  .l-name{
+    font-weight: bold;
+    font-size: 24px;
+    color: #CFE2FF;
+  }
 }
 
 .wrapper{
